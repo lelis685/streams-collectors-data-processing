7a45026a83827ac5343cf3eca2eb9409cbb3efc0
lelis <lelis685@gmail.com>
2023-03-05 13:11:11 -0300

    config inicial

---
+++ src/main/java/com/streams/dataprocessing/demo/DataprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.streams.dataprocessing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataprocessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streams/dataprocessing/demo/DataprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streams.dataprocessing.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
